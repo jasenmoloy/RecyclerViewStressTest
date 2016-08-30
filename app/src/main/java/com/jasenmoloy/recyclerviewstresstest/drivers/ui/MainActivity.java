42c1be5d9dcb41f10b87ab9326bdab777907e3ee
Jasen Moloy <jasenmoloy.dev@gmail.com>
2016-08-29 20:30:24 -0700

    1) Change look and feel of cards and recycler view 2) Add design library and update all libs to use version 24 3) Replace Main Activity with CoordinatorLayout and add AppBarLayout to project

--- app/src/main/java/com/jasenmoloy/recyclerviewstresstest/drivers/ui/MainActivity.java
+++ app/src/main/java/com/jasenmoloy/recyclerviewstresstest/drivers/ui/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.widget.Toast;
 
 import com.jasenmoloy.recyclerviewstresstest.R;
@@ -21,11 +22,13 @@ public class MainActivity extends AppCompatActivity implements MainView {
     MainRecyclerViewAdapter recyclerAdapter;
     MainPresenter presenter;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.mainactivity_toolbar);
+        setSupportActionBar(toolbar);
 
         recyclerView = (RecyclerView) findViewById(R.id.mainactivity_recyclerview);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -46,6 +49,17 @@ public class MainActivity extends AppCompatActivity implements MainView {
         super.onStart();
 
         presenter.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
     }
 
     @Override
